bb6382048becde6c342dfd351f11ccde7cb5423b
tiyo.lab <tiyo.lab@gmail.com>
2017-01-27 14:37:29 +0700

    test 1

--- app.js
+++ app.js
@@ -81,6 +81,7 @@ app.get('/webhook', function(req, res) {
  *
  */
 app.post('/webhook', function (req, res) {
+	console.log('receive requests');
   var data = req.body;
 
   // Make sure this is a page subscription
