e891ac7f881ab0a66dfda397e6d1baa45480eb82
tiyo.lab <tiyo.lab@gmail.com>
2017-02-01 17:19:59 +0700

    acccount linking

--- app.js
+++ app.js
@@ -344,7 +344,7 @@ function sendShowBrokerMessage(recipientId){
                 }
                 
                 
-                String street = '';
+                var street = '';
                 if(ac.BillingStreet){
                 	street = BillingStreet;
                 }
