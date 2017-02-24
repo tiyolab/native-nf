ddcd29260eba61506ba775eee8e1345ca993bd1f
tiyo.lab <tiyo.lab@gmail.com>
2017-02-24 14:09:55 +0700

    add configuration

--- my_modules/my_request.js
+++ my_modules/my_request.js
@@ -6,14 +6,13 @@ exports.handleRequest = (app, db) => {
 	 */
 	app.get('/configure', function(req, res){
 		var collection = db.collection('app_configuration');
-		var configuration = collection.findOne({
-			app_name: req.header.host
+		collection.findOne({
+			app_name: req.headers.host
 		}, function(err, item){
 			console.log('result');
 			console.log(item);
 		});
-		console.log(req.header.host);
-		console.log(configuration);
+		console.log(req.headers.host);
 		res.render('configure');
 	});
 	
