8f2a32d803b9373ba9af6880cee4adb24a0ff5ca
tiyo.lab <tiyo.lab@gmail.com>
2017-02-24 17:36:28 +0700

    add configuration

--- my_modules/my_config.js
+++ my_modules/my_config.js
@@ -25,7 +25,7 @@ exports.configure = (db) => {
 			console.log('ERR: cannot load configuration');
 			return;
 		}else{
-			console.log(item);
+			
 			if(item){
 				app_name 					= item.app_name;
 				host 						= item.host;
@@ -43,6 +43,7 @@ exports.configure = (db) => {
 				console.log('ERR: configuration not exists, configure by access /configure');
 				return;
 			}
+			console.log(sf_consumer_key);
 		}
 	});
 }
