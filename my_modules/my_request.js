465a292ee52154b979edca45cddd4d13ba111753
tiyo.lab <tiyo.lab@gmail.com>
2017-02-24 11:22:37 +0700

    add configuration

--- my_modules/my_request.js
+++ my_modules/my_request.js
@@ -1,10 +1,16 @@
 
-exports.handleRequest = (app) => {
+exports.handleRequest = (app, db) => {
 	/**
 	 * This section for configuration only
 	 * 
 	 */
 	app.get('/configure', function(req, res){
+		var configuration = db.app_configuration.findOne({
+			app_name: req.header.host
+		});
+		console.log(req.header.host);
+		console.log(conviguration);
 		res.render('configure');
 	});
+	
 }
