34155aa430afed626f888d2a4e6fb41350efc91d
tiyo.lab <tiyo.lab@gmail.com>
2017-02-24 14:52:12 +0700

    add configuration

--- my_modules/my_request.js
+++ my_modules/my_request.js
@@ -18,26 +18,13 @@ exports.handleRequest = (app, db) => {
 	});
 	
 	app.post('/configure', function(req, res){
+		var collection = db.collection('app_configuration');
 		var data = req.body;
-		console.log(data);
-		/*var configureData = {
-				app_name : data.app_name,
-				host: req.headers.host,
-				salesforce_app: {
-					consumer_key: data.consumer_key,
-					consumer_secret: data.consumer_secret,
-					username: data.username,
-					password: data.password
-				},
-				facebook_app:{
-					app_name: data.app_name,
-					app_id: data.app_id,
-					app_secret: data.app_secret,
-					webhook_validation_token: data.webhook_validation_token,
-					page_name: data.page_name,
-					page_access_token: data.page_access_token
-				}
-			}*/
 		
+		data['host'] = req.headers.host;
+		collection.insert(data, {w:1}, function(err, result){
+			if(err) alert('operation failed');
+			else alert('operation success');
+		});
 	});
 }
